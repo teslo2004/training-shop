b4de315229f80eb952360621a29b70f3206128f8
teslo2004 <teslo2004@gmail.com>
2022-02-24 16:49:57 +0300

    update adaptive

--- src/components/Clothes/Clothes-cart/ClothesCart.jsx
+++ src/components/Clothes/Clothes-cart/ClothesCart.jsx
@@ -5,18 +5,20 @@ import './clothesCart.scss';
 
 export const ClothesCart = ({ card: { img, name, price, raiting, sale, id }, productType }) => {
   return (
-    <Link to={`/${productType}/${id}`} data-test-id={`clothes-card-${productType}`}>
-      <div className="clothes-img">
-        <img src={img} alt={img} />
-      </div>
-      <div className="clothes-name">{name}</div>
-      <div className="clothes-footer">
-        <div className="clothes-price">$ {price} </div>
-        <span className="clothes-sale">{sale && '$ ' + (100 / parseFloat(sale)) * price}</span>
-        <div className="clothes-raiting">
-          <Raiting raiting={raiting} />
+    <div className="clothes-carts">
+      <Link to={`/${productType}/${id}`} data-test-id={`clothes-card-${productType}`}>
+        <div className="clothes-img">
+          <img src={img} alt={img} />
         </div>
-      </div>
-    </Link>
+        <div className="clothes-name">{name}</div>
+        <div className="clothes-footer">
+          <div className="clothes-price">$ {price} </div>
+          <span className="clothes-sale">{sale && '$ ' + (100 / parseFloat(sale)) * price}</span>
+          <div className="clothes-raiting">
+            <Raiting raiting={raiting} />
+          </div>
+        </div>
+      </Link>
+    </div>
   );
 };
