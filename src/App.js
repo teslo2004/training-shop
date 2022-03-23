1d901f5c9d6835ce6fe897cd47eaff473037be11
teslo2004 <teslo2004@gmail.com>
2022-03-23 20:01:36 +0300

    update app

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes, useLocation } from 'react-router-dom';
 import { MainPage } from './page/MainPage/MainPage';
 import { ProductsPage } from './page/ProductsPage/ProductsPage';
 import { ProductPage } from './page/ProductPage/ProductPage';
+
 import './app.scss';
 import { useEffect } from 'react';
 import Spinner from './components/Spinner/Spinner';
